84d7de73607c6e5fe2fa27e660be21045dcc244d
Jarek Páral <paral.jarek@gmail.com>
2018-03-13 21:24:54 +0100

    Add example: servo control by potentiometer

---
+++ serva03-pot/serva03-pot.ino
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+
+#define L_RGB_R 5
+#define L_RGB_G 11
+#define L_RGB_B 6
+
+#define L_R 12
+#define L_Y 13
+#define L_G 7
+#define L_B 8
+
+#define SW1 2
+#define SW2 3
+#define SW3 4
+
+#define PIEZO_A 9
+#define PIEZO_B 10
+
+#define POT1 A0
+#define POT2 A1
+
+#define PHOTO A2
+
+#define DHTPIN A3
+#define DHTTYPE DHT11
+
+#define S1 5
+#define S2 6
+#define S3 9
+#define S4 10
+#define S5 11
+
+#include <Servo.h> 
+
+Servo myservo;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(L_RGB_R, OUTPUT);
+  pinMode(L_RGB_G, OUTPUT);
+  
+  Serial.println("Start ServoTest");
+  myservo.attach(S5);
+  Serial.println("ServoTest - attach");
+  myservo.write(90);
+  Serial.println("ServoTest - write");
+  //delay(1000);
+  Serial.println("ServoTest - continue");
+}
+
+int motor = 0;
+void loop() {   
+  motor = analogRead(POT1) * 0.175;
+  Serial.println(motor); // 0 - 1023
+  myservo.write(motor); // write(0 - 180)
+  analogWrite(L_RGB_G, motor); // analogWrite 0 - 255  delay(100);
+}
